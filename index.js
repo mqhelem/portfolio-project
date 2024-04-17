be45d105b16a57d5ff18bbdd26aa206f030abfa4
mqhelem <155308250+mqhelem@users.noreply.github.com>
2024-04-17 13:30:43 +0200

    contacts

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function contactFeedback(event){
-    event.preventDefault()
+function contactFeedback() {
   let names = document.querySelector("#names");
   let email = document.querySelector("#email");
   let enquiry = document.querySelector("#enquiry");
-  if (names.length && email.length && enquiry.length) {
+  if (names.length & email.length & enquiry.length) {
     alert(
       "Thank you for submitting an enquiry, one of our Consultants will get back to you"
     );
+  } else {
+    alert("We need you to input all your details");
   }
 }
-let btn submit = document.querySelector(".submit")
-btn submit.addEventListener("click, contactFeedback")
+let button = document.querySelector("button");
+button.addEventListener("click", contactFeedback);
